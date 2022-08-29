f86db36f4bee311eb9dbe5fa8e147cb3aeece98f
ross <rosko48@gmail.com>
2022-08-29 20:35:56 +0300

    creating project structure

---
+++ src/test/java/homePageTest/HomePageTest.java
@@ -0,0 +1,17 @@
+package homePageTest;
+
+import baseTest.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HomePageTest extends BaseTest {
+
+    @Test
+    public void openLoginPage(){
+
+        homePage.openHomePage();
+        Assert.assertTrue("Home page is not opened", homePage.isHomePageOpened());
+    }
+
+
+}
