eca4b071d3692ac37f1f7b43687d7388629861b7
RossKoll <rosko48@gmail.com>
2022-11-17 21:24:23 +0200

    fix2

--- src/test/java/textBoxPageTest/TextBoxPageTest.java
+++ src/test/java/textBoxPageTest/TextBoxPageTest.java
@@ -25,17 +25,10 @@ public class TextBoxPageTest extends BaseTest {
                 .clickOnSubmitButton();
         Assert.assertTrue(textBoxPage.getResultTable().isDisplayed());
 
-
         Assert.assertTrue("Username output does not match", textBoxPage.getNameOutput().getText().contains(username));
         Assert.assertTrue("Email output does not match", textBoxPage.getEmailOutput().getText().contains(email));
         Assert.assertTrue("Current address output does not match", textBoxPage.getCurrentAddressOutput().getText().contains(currentAddress));
-
-//        Thread.sleep(2000);
-//        String curAd = textBoxPage.getCurrentAddressOutput().getAccessibleName();
-//        Assert.assertTrue("Current Address output does not match", textBoxPage.getCurrentAddressOutput().getText().contains(currentAddress));
-//
-//        String vareturnValur = textBoxPage.getPermanentAddressOutput().getText();
-       Assert.assertTrue("Permanent address output does not match", textBoxPage.getPermanentAddressOutput().getText().contains(permanentAddress));
+        Assert.assertTrue("Permanent address output does not match", textBoxPage.getPermanentAddressOutput().getText().contains(permanentAddress));
         logger.info(" Time to runt test is : " + (System.currentTimeMillis()-start));
     }
 
